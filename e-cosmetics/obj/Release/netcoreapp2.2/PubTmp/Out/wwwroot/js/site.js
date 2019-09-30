1af1d64a9d792e2a44342c6b8bc6a71729179817
alenSavov <alensavov@gmail.com>
2019-09-30 21:58:01 +0300

    Added Basic styles for social links

--- e-cosmetics/obj/Release/netcoreapp2.2/PubTmp/Out/wwwroot/js/site.js
+++ e-cosmetics/obj/Release/netcoreapp2.2/PubTmp/Out/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function show() {
+    document.getElementById('nav-toggle')
+        .classList.toggle('active');
+}
+
+function closeToggle() {
+    document.getElementById('nav-toggle')
+        .classList.remove('active');
+}
