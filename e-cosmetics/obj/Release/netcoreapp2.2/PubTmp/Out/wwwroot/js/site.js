7d8fa00fbbf0b699903001151a8a4621a7d3d916
alenSavov <alensavov@gmail.com>
2019-10-03 23:11:46 +0300

    Added styles for toggle menu

--- e-cosmetics/obj/Release/netcoreapp2.2/PubTmp/Out/wwwroot/js/site.js
+++ e-cosmetics/obj/Release/netcoreapp2.2/PubTmp/Out/wwwroot/js/site.js
@@ -1,14 +1,24 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+/*Hamburger*/
+$(document).ready(function () {
+    $('.icon').click(function () {
+        $('.icon').toggleClass('active');
 
-// Write your JavaScript code.
+        setTimeout(function () {
+            document.getElementById('nav-toggle')
+                .classList.toggle('active');
+        }, 500);
+    })
+})
 
-function show() {
-    document.getElementById('nav-toggle')
-        .classList.toggle('active');
-}
+//function show() {
+//    document.getElementById('nav-toggle')
+//        .classList.toggle('active');
+//}
 
 function closeToggle() {
     document.getElementById('nav-toggle')
         .classList.remove('active');
+
+    $('.icon').toggleClass('active');
 }
