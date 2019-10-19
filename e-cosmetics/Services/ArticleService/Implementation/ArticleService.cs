e16d142836905ad20bd5e262b20e3ecaf602d735
alenSavov <alensavov@gmail.com>
2019-10-19 20:17:55 +0300

    Added ArticleService

---
+++ e-cosmetics/Services/ArticleService/Implementation/ArticleService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace e_cosmetics.Services.ArticleService.Implementation
+{
+    public class ArticleService
+    {
+    }
+}
