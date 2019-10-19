f156a936579393a842071f1421e28cc2af562ad0
alenSavov <alensavov@gmail.com>
2019-10-19 20:04:42 +0300

    Changed controller name to article

---
+++ e-cosmetics/Controllers/ArticleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace e_cosmetics.Controllers
+{
+    public class ArticleController: Controller
+    {
+        public IActionResult GetAll()
+        {
+            return View();
+        }
+    }
+}
