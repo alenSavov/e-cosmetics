8ce26db230e333c3348673acec6948b3840b9a94
alenSavov <alensavov@gmail.com>
2019-10-26 15:35:55 +0300

    Added styles for article

--- e-cosmetics/Services/Articles/Models/ArticleViewModel.cs
+++ e-cosmetics/Services/Articles/Models/ArticleViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using e_cosmetics.Services.Pictures.Models;
 
 namespace e_cosmetics.Services.Articles.Models
@@ -13,9 +14,9 @@ namespace e_cosmetics.Services.Articles.Models
         public string Creator { get; set; }
 
         //.ToString("MMMM dd, yyyy")
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{MMMM dd, yyyy}")]
+        //[DisplayFormat(ApplyFormatInEditMode = true, "MM/dd/yyyy", CultureInfo.InvariantCultur)]
         public DateTime CretedOn { get; set; }
-
+        
         public string Content { get; set; }
 
         public BasePictureViewModel Picture { get; set; }
